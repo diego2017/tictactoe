c41d79f2ef58d7f4b1af46b34acb3804738950e4
Diego <diego_v_f@hotmail.com>
2017-03-01 16:50:03 +1100

    working with easy AI

--- js/main.js
+++ js/main.js
@@ -4,12 +4,14 @@ $(document).ready(function(){
   $('.board').hide();
   $('.options2').hide();
   window.activeAi = false;
-
-  var aiLevel = 'easy';
+  var counter = 0;
+  var aiLevel = 'hard';
   var usersSelected = 0;
 
-  window.game = {
 
+  window.player = 0;
+
+  window.game = {
     board : [
       [null,null,null],
       [null,null,null],
@@ -49,6 +51,7 @@ $(document).ready(function(){
           winner = game.players[player].name
           console.log('THE WINNER IS: ' + winner);
           alert ('THE WINNER IS: ' + winner);
+          window.player = 0;
           window.youWon();
 
         };
@@ -71,7 +74,7 @@ $(document).ready(function(){
 
 ////  Events ////
 
-      var player = 0 ;
+      window.player = 0 ;
       $('.board td').on("click", function(){
         var row = $(this).attr('row');
         var col = $(this).attr('column');
@@ -79,10 +82,15 @@ $(document).ready(function(){
         game.modifyBoard(player,row,col);
         $(this).addClass(game.players[player].image);
 
-        if (player === 0 ){
-          player = 1
+      /// delete this, just for testing
+        counter = counter + 1;
+        console.log(counter);
+      ///
+
+        if (window.player === 0 ){
+          window.player = 1
         } else {
-          player = 0
+          window.player = 0
           if (activeAi === true) {
             window.aI();
         };
@@ -96,7 +104,9 @@ $(document).ready(function(){
         window.setTimeout(function () {
           $("td").removeClass( );
         }, 2000);
-        usersSelected = 0;
+        usersSelected = 2;
+        debugger;
+        window.player= 0;
       };
 
 
@@ -153,7 +163,6 @@ $(document).ready(function(){
     });      // btn playerRobot
 
 /// AI section - Robot vs Player
-
   window.createMove = function(){
 
     if (aiLevel === 'easy'){
@@ -166,6 +175,88 @@ $(document).ready(function(){
         return createMove();
       }
     }          // aiLevel: easy
+    //
+    if (aiLevel === 'hard'){
+      var boardSummary = window.game.board.toString();
+      if (boardSummary === ",,,,,,,,"){
+        $('td#00').trigger( "click" );
+      }
+      else if ( ((game.board[0][0] === 0 )&&(game.board[0][2] === 0 )&&(game.board[0][1]===null)) ||
+                ((game.board[1][1] === 0 )&&(game.board[2][1] === 0 )&&(game.board[0][1]===null)) ){
+        $('td#01').trigger( "click" );
+      }
+      else if ( ((game.board[0][0] === 0 )&&(game.board[0][1] === 0 )&&(game.board[0][2]===null)) ||
+                ((game.board[1][2] === 0 )&&(game.board[2][2] === 0 )&&(game.board[0][2]===null)) ||
+                ((game.board[2][0] === 0 )&&(game.board[1][1] === 0 )&&(game.board[0][2]===null)) ){
+        $('td#02').trigger( "click" );
+      }
+      else if ( ((game.board[0][0] === 0 )&&(game.board[2][0] === 0 )&&(game.board[1][0]===null)) ||
+                ((game.board[1][1] === 0 )&&(game.board[1][2] === 0 )&&(game.board[1][0]===null)) ){
+        $('td#10').trigger( "click" );
+      }
+      else if ( ((game.board[1][0] === 0 )&&(game.board[1][2] === 0 )&&(game.board[1][1]===null)) ||
+                ((game.board[0][1] === 0 )&&(game.board[2][1] === 0 )&&(game.board[1][1]===null)) ||
+                ((game.board[2][0] === 0 )&&(game.board[0][2] === 0 )&&(game.board[1][1]===null)) ||
+                ((game.board[0][2] === 0 )&&(game.board[2][2] === 0 )&&(game.board[1][1]===null)) ){
+        $('td#11').trigger( "click" );
+      }
+      else if ( ((game.board[0][2] === 0 )&&(game.board[2][2] === 0 )&&(game.board[1][2]===null)) ||
+                ((game.board[1][0] === 0 )&&(game.board[1][1] === 0 )&&(game.board[1][2]===null)) ){
+        $('td#12').trigger( "click" );
+      }
+      else if ( ((game.board[0][0] === 0 )&&(game.board[1][0] === 0 )&&(game.board[2][0]===null)) ||
+                ((game.board[2][1] === 0 )&&(game.board[2][2] === 0 )&&(game.board[2][0]===null)) ||
+                ((game.board[0][2] === 0 )&&(game.board[1][1] === 0 )&&(game.board[2][0]===null)) ){
+        $('td#20').trigger( "click" );
+      }
+      else if ( ((game.board[2][0] === 0 )&&(game.board[2][2] === 0 )&&(game.board[2][1]===null)) ||
+                ((game.board[0][1] === 0 )&&(game.board[1][1] === 0 )&&(game.board[2][1]===null)) ){
+        $('td#21').trigger( "click" );
+      }
+      else if ( ((game.board[2][0] === 0 )&&(game.board[2][1] === 0 )&&(game.board[2][2]===null)) ||
+                ((game.board[0][2] === 0 )&&(game.board[0][1] === 0 )&&(game.board[2][2]===null)) ||
+                ((game.board[0][0] === 0 )&&(game.board[1][1] === 0 )&&(game.board[2][2]===null)) ){
+        $('td#22').trigger( "click" );
+      }
+      else if ( (boardSummary === "0,1,,,,,,,") ||
+                (boardSummary === "0,,,1,,,,,") ||
+                (boardSummary === "0,,,,,1,,,") ||
+                (boardSummary === "0,,,,,,,1,") ){
+        $('td#11').trigger( "click" );
+      }
+      else if ( (boardSummary === "0,1,,,0,,,,1") ||
+                (boardSummary === "0,,,1,0,,,,1") ||
+                (boardSummary === "0,,,,0,1,,,1") ||
+                (boardSummary === "0,,,,0,,,1,1") ){
+        $('td#20').trigger( "click" );
+      }
+      else if ( (boardSummary === "0,,1,,,,,,") ||
+                (boardSummary === "0,,,,,,,,1")){
+        $('td#20').trigger( "click" );
+      }
+      else if ( (boardSummary === "0,,,,,,1,,")){
+        $('td#22').trigger( "click" );
+      }
+      else if ( (boardSummary === "0,,1,1,,,0,,")){
+        $('td#22').trigger( "click" );
+      }
+      else if ( (boardSummary === "0,,,1,,,0,,1")){
+        $('td#02').trigger( "click" );
+      }
+      else if ( (boardSummary === "0,,,,1,,,,")){
+        $('td#22').trigger( "click" );
+      }
+      else if ( (boardSummary === "0,,1,,1,,,,0")){
+        $('td#20').trigger( "click" );
+      }  //"0,,,,1,,1,,0"
+      else if ( (boardSummary === "0,,,,1,,1,,0")){
+        $('td#02').trigger( "click" );
+      }
+      else {
+        aiLevel = 'easy';
+        return createMove();
+      };
+    }         // aiLevel hard
   };       // createMove
 
   window.aI = function (){
