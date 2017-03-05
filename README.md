110a4f554e1898aa06328ae9994e1e501261e78e
Diego <diego_v_f@hotmail.com>
2017-03-05 20:17:02 +1100

    Sun 5 Mar version. README completed

--- README.md
+++ README.md
@@ -1,23 +1,37 @@
-#Title: Tic Tac Toe
+# Tic Tac Toe
 
-###Type:
-- Project 0 from WDi GA
-
-###Summary
-- This application is a Tic Tac Toe game.
+## Summary
+- This application is a Tic Tac Toe game. :video_game:
 - The folder contains an index.html file, one css which includes a file called style.css, a js folder with 2 files: jquery.js and main.js, and an images folder with 7 images, one sound called 'zeldaSound', and a README.md file.
+- This application was created by Diego Villanueva for Project 0 - WDi20 General Assembly
 
-###Online version
-An online version of the game can be accessed using this  [Link](https://diego2017.github.io/tictactoe/)
+### Online version
+An online version of the game can be accessed using this  [Link  :link:](https://diego2017.github.io/tictactoe/)
 
-###Objective of the game:
+### Objective of the game:
 - In the game, there are 2 players who take turns selecting a space in a 3×3 grid. Every time a player selects a space, the space changes to their selected image. In order to win, players should place three of their images in a horizontal, vertical, or diagonal row.
 
-###Instructions of the game:
-- Select whether you want to play against the computer, or 2 players against each other. After this section, choose each player has to choose an image and input their name. The computer will choose its own image.
-- Play the game by clicking on one square.
-- To restart the game, click 'Start Again'
+### Instructions of the game:
+* Select whether you want to play:
+  * Player against the computer :man: vs :computer:
+  * Player against other player :man: vs :man:
+* After this section, choose each player has to choose an image and input their name. The computer will choose its own image.
+* Play the game by clicking on one square.
+* To restart the game, click 'Start Again'
 
-###Specifications:
-* For this application, the code has been written in 3 files: one html file (index.html), one javascript file (js/main.js), and one css file (css/style.css).
-*
+## Specifications:
+For this application, the code has been written in 3 files: one html file (index.html), one javascript file (js/main.js), and one css file (css/style.css).
+
+#### main.js
+* Prepare the page to be shown to user. It hides sections not shown when the page is loaded.
+* Declaration of variables
+* Object window.game - Includes the board used to track players' movements, data of each player(player number, name, token to be used when images don't work, and the image that will be shown on the square selected by players), a findWinner function (reviews the board in game and checks if anybody has won), and a modifyBoard function that changes the board when the player number, and position on the board are provided.
+* Board event listener, for the squared selected by players.
+* Other functions. Includes resetGame (function used to clear the board inside the game object and the one displayed to players), youWon (function used to show celebration gifs).
+* Event listeners for 4 buttons selected by players: playerPlayer (when the player vs player button is clicked), userOptions (for the players to select their image), startAgain (for reseting the game), and playerRobot (when the player vs robot button is clicked)
+* AI section used when Robot vs Player is selected. There is one function to create positions on the board and a function to select the square.
+
+## Comments on the project and its development:
+* The game is working for 2 players and for player vs computer. The AI section is using conditionals based on a strategy to win. When there is no strategy to follow, the function created selects a random available square.
+* During the development process I found that in the beginning I had an idea on how to create the application but once I had more code. My plan had to be adapted and I started adding other functions to achieve the result I wanted.
+* The biggest problem I had was that after adding various functions and variables, I started getting confused with the functions and when I had bugs, it was hard to find the solution because many functions were linked and depended on each other.
--- js/main.js
+++ js/main.js
@@ -1,31 +1,29 @@
 $(document).ready(function(){
-
+  // preparing the page to be shown to user. It hides sections not shown when the page is loaded
   $('.selectUser').hide()
   $('.board').hide();
   $('.options2').hide();
   $('h2#points').hide();
   $('h2#round').hide();
-  window.activeAi = false;
-  window.counter = 0;
-  var aiLevel = 'hard';
-  var usersSelected = 0;
+  // declaration of variables
+  window.activeAi = false;    // Computer player is inactive when the page is loaded
+  window.counter = 0;         // Counts the moves done by players. Used to reset after draw
   window.round = 1;
-  window.winsPlayer0 = 0;
-  window.winsPlayer1 = 0;
-
+  window.winsPlayer0 = 0;     // Times player0 has won
+  window.winsPlayer1 = 0;     // Times player1 has won
   window.gameOver = false;
   window.draw = false;
+  window.player = 0;          // Player 0 always starts playing
+  var aiLevel = 'hard';       // Setting AI level to hard
+  var usersSelected = 0;      // Used to determine when both players are selected, or 1 player with AI
 
-
-  window.player = 0;
-
+  // Object window.game
   window.game = {
     board : [
       [null,null,null],
       [null,null,null],
       [null,null,null]
     ],
-
     players : [
       {
         player: 0,
@@ -40,21 +38,21 @@ $(document).ready(function(){
         image: 'three'
       }
     ],
-
     findWinner : function (){
       var winner = null;
       this.players.forEach(function(player){
         player = player.player;
-        if ((game.board[0][0]===player) && (game.board[0][1]===player) && (game.board[0][2]===player)||
-            (game.board[1][0]===player) && (game.board[1][1]===player) && (game.board[1][2]===player)||
-            (game.board[2][0]===player) && (game.board[2][1]===player) && (game.board[2][2]===player)||
+        if (
+          (game.board[0][0]===player) && (game.board[0][1]===player) && (game.board[0][2]===player)||
+          (game.board[1][0]===player) && (game.board[1][1]===player) && (game.board[1][2]===player)||
+          (game.board[2][0]===player) && (game.board[2][1]===player) && (game.board[2][2]===player)||
 
-            (game.board[0][0]===player) && (game.board[1][0]===player) && (game.board[2][0]===player)||
-            (game.board[0][1]===player) && (game.board[1][1]===player) && (game.board[2][1]===player)||
-            (game.board[0][2]===player) && (game.board[1][2]===player) && (game.board[2][2]===player)||
+          (game.board[0][0]===player) && (game.board[1][0]===player) && (game.board[2][0]===player)||
+          (game.board[0][1]===player) && (game.board[1][1]===player) && (game.board[2][1]===player)||
+          (game.board[0][2]===player) && (game.board[1][2]===player) && (game.board[2][2]===player)||
 
-            (game.board[0][0]===player) && (game.board[1][1]===player) && (game.board[2][2]===player)||
-            (game.board[2][0]===player) && (game.board[1][1]===player) && (game.board[0][2]===player)
+          (game.board[0][0]===player) && (game.board[1][1]===player) && (game.board[2][2]===player)||
+          (game.board[2][0]===player) && (game.board[1][1]===player) && (game.board[0][2]===player)
         ) {
           winner = game.players[player].name
           if (player===0){
@@ -70,14 +68,11 @@ $(document).ready(function(){
           console.log('THE WINNER IS: ' + winner);
           window.player = 0;
           window.gameOver = true;
-
         } else if (counter >7){
-          // debugger;
           window.draw = true;
         };
-      });
+      }); // closing this.players.forEach function
     },    //close findWinner
-
     modifyBoard : function (player, row, column){
       if (this.board[row][column] === null){
         this.board[row][column] = player;
@@ -85,145 +80,131 @@ $(document).ready(function(){
       console.log(game.board[0]);
       console.log(game.board[1]);
       console.log(game.board[2]);
-      this.findWinner();
+      this.findWinner();   // to check if anybody has won
     }
   };  // end of game object ////
 
-
-
-////  Events ////
-
-      $('.board td').on("click", function(){
-        var row = $(this).attr('row');
-        var col = $(this).attr('column');
-        console.log(row + ", " + col);
-        // debugger;
-        if (window.game.board[row][col] === null){
-          $(this).addClass(window.game.players[player].image);
-          window.game.modifyBoard(player,row,col);
-          window.counter +=1;
-          console.log(window.counter);
-          if (window.player === 0 ){
-            window.player = 1
-          } else {
-            window.player = 0
-            if (activeAi === true) {
-              window.aI();
-            }
-          }
-        };
-        if( gameOver ){
-          window.youWon(); // also resetGame
-          return;
-        };
-        if( draw ){
-          window.draw = false;
-          $('#messages').html(' Draw ')
-          window.setTimeout(function () {
-            $('#messages').html('')
-          }, 2000);
-          resetGame();
-          return;
+  // This section includes the event listener for the squares to be selected by players,
+  $('.board td').on("click", function(){
+    var row = $(this).attr('row');
+    var col = $(this).attr('column');
+    console.log(row + ", " + col);
+    if (window.game.board[row][col] === null){     // checks that the square hasn't been selected
+      $(this).addClass(window.game.players[player].image);  //square shows player's image
+      window.game.modifyBoard(player,row,col);  //modify the board inside game.board
+      window.counter +=1;
+      console.log(window.counter);
+      if (window.player === 0 ){
+        window.player = 1
+      } else {
+        window.player = 0
+        if (activeAi === true) {  // If Player vs Robot is selected, aI will play
+          window.aI();
         }
-      });       // closing $('.board td').on("click"...
+      }
+    };
+    if( gameOver ){
+      window.youWon(); // also function resetGame which is called from youWon
+      return;
+    };
+    if( draw ){   // when nobody has won but all squares have been selected
+      window.draw = false;
+      $('#messages').html(' Draw ')
+      window.setTimeout(function () {
+        $('#messages').html('')
+      }, 2000);
+      resetGame();
+      return;
+    }
+  });       // closing $('.board td').on("click"...
 
-      window.resetGame = function (){
-        window.game.board = [
-          [null,null,null],
-          [null,null,null],
-          [null,null,null]
-        ];
-        window.round += 1;
-        var aiLevel = 'hard';
-        window.setTimeout(function () {
-          $("td").removeClass( );
-          // continue new game
-          if (activeAi === true) {
-            console.log('playing first AI move in new game');
-            window.aI();
-          }
-        }, 2000);
-        window.player= 0;
-        window.counter=0;
-        window.gameOver = false;
-        $('h2#round').html('Round ' + window.round)
-        $('h2#points').html(game.players[0].name + ': ' +  window.winsPlayer0 + '  -  ' + game.players[1].name + ': ' + window.winsPlayer1 );
-        // debugger;
-      };
+  // Functions. Includes resetGame & youWon
+  window.resetGame = function (){
+    window.game.board = [
+      [null,null,null],
+      [null,null,null],
+      [null,null,null]
+    ];
+    window.round += 1;
+    var aiLevel = 'hard';
+    window.setTimeout(function () {
+      $("td").removeClass( );
+      // continue new game
+      if (activeAi === true) {
+        window.aI();  //playing first AI move in new game'
+      }
+    }, 2000);
+    window.player= 0;
+    window.counter=0;
+    window.gameOver = false;
+    // Info displayed to players: round number and games won by each player
+    $('h2#round').html('Round ' + window.round)
+    $('h2#points').html(game.players[0].name + ': ' +  window.winsPlayer0 + '  -  ' + game.players[1].name + ': ' + window.winsPlayer1 );
+  };
+  window.youWon = function () {
+    $("td").removeClass( );
+    $("td").addClass("win");
+    window.resetGame();
+  };
 
-
-      window.youWon = function () {
-        $("td").removeClass( );
-        $("td").addClass("win");
-        window.resetGame();
-      };
-
-///////// user selection
-
-    $('#playerPlayer').on("click", function() {
-        $('.selectUser').show();
-        $('.userOptions td').show();
-        $('.options').hide();
-    });
-
-    $('.userOptions td').on("click", function() {    //select players
-
-        window.game.players[usersSelected].image =  $(this).attr('class');
-        var name = window.prompt("What is your name? "," ");
-        window.game.players[usersSelected].name = name;
-        $(this).hide();
-
-        usersSelected = usersSelected + 1;
-
-        if (usersSelected >1){  //once both selected
-          $('.selectUser').hide();  //hide options
-          $('.options').hide();
-          $('.options2').show();
-          $('.board').show();
-          $('h2#points').show();
-          $('h2#round').show();
-          $('h2#points').html(game.players[0].name + ': 0  -  ' + game.players[1].name + ': 0');
-          if (window.activeAi === true){
-            window.aI();
-          };
-        }
-    });
-
-    $('#startAgain').click(function() {
-      location.reload();
-    });
-
-    $('#playerRobot').click(function() {
-      activeAi = true;
+ //Event listeners for 4 buttons selected by players: playerPlayer, userOptions, startAgain and playerRobot
+  $('#playerPlayer').on("click", function() {
       $('.selectUser').show();
+      $('.userOptions td').show();
       $('.options').hide();
-      $('.userOptions td').show()
-      window.game.players[0].image =  'robot';
-      window.game.players[0].name = 'Robot';
-      usersSelected = 1;
+  });   // close playerPlayer
+  $('.userOptions td').on("click", function() {    //select players
+      window.game.players[usersSelected].image =  $(this).attr('class');
+      var name = window.prompt("What is your name? "," ");
+      window.game.players[usersSelected].name = name;
+      $(this).hide();
+      usersSelected = usersSelected + 1;
+      if (usersSelected >1){  //once both selected
+        $('.selectUser').hide();  //hide options
+        $('.options').hide();
+        $('.options2').show();
+        $('.board').show();
+        $('h2#points').show();
+        $('h2#round').show();
+        $('h2#points').html(game.players[0].name + ': 0  -  ' + game.players[1].name + ': 0');
+        if (window.activeAi === true){
+          window.aI();
+        };
+      }
+  });   // close userOptions
+  $('#startAgain').click(function() {
+    location.reload();
+  });
+  $('#playerRobot').click(function() {
+    activeAi = true;
+    $('.selectUser').show();
+    $('.options').hide();
+    $('.userOptions td').show()
+    window.game.players[0].image =  'robot';
+    window.game.players[0].name = 'Robot';
+    usersSelected = 1;
 
-    });      // btn playerRobot
+  });      // close playerRobot
 
-/// AI section - Robot vs Player
-  window.createMove = function(){
-
+  //AI section - When Robot vs Player is selected
+  window.createMove = function(){   // function to create positions on board. options: easy and hard
     if (aiLevel === 'easy'){
       var row = (Math.round(Math.random() * 2)).toString();
       var col = (Math.round(Math.random() * 2)).toString();
       if (window.game.board[row][col] == null){
         var selected = row+col;
+        aiLevel = 'hard';
         return selected;
       }else{
         return createMove();
       }
-    }          // aiLevel: easy
-    //
+    };  // close aiLevel: easy
     if (aiLevel === 'hard'){
       var boardSummary = window.game.board.toString();
-      // debugger;
       if (boardSummary === ",,,,,,,,"){
         $('td#00').trigger( "click" );
       }
+      // square to select when winning is possible
       else if ( ((game.board[0][0] === 0 )&&(game.board[0][2] === 0 )&&(game.board[0][1]===null)) ||
                 ((game.board[1][1] === 0 )&&(game.board[2][1] === 0 )&&(game.board[0][1]===null)) ){
         $('td#01').trigger( "click" );
@@ -261,7 +242,7 @@ $(document).ready(function(){
                 ((game.board[0][0] === 0 )&&(game.board[1][1] === 0 )&&(game.board[2][2]===null)) ){
         $('td#22').trigger( "click" );
       }
-    //// to prevent player wins
+      //// moves to prevent that player wins
       else if ( ((game.board[0][0] === 1 )&&(game.board[0][2] === 1 )&&(game.board[0][1]===null)) ||
                 ((game.board[1][1] === 1 )&&(game.board[2][1] === 1 )&&(game.board[0][1]===null)) ){
         $('td#01').trigger( "click" );
@@ -299,7 +280,7 @@ $(document).ready(function(){
                 ((game.board[0][0] === 1 )&&(game.board[1][1] === 1 )&&(game.board[2][2]===null)) ){
         $('td#22').trigger( "click" );
       }
-      ////////
+      // specific moves to win
       else if ( (boardSummary === "0,1,,,,,,,") ||
                 (boardSummary === "0,,,1,,,,,") ||
                 (boardSummary === "0,,,,,1,,,") ||
@@ -333,26 +314,24 @@ $(document).ready(function(){
       }
       else if ( (boardSummary === "0,,1,,1,,,,0")){
         $('td#20').trigger( "click" );
-      }  //"0,,,,1,,1,,0"
+      }
       else if ( (boardSummary === "0,,,,1,,1,,0")){
         $('td#02').trigger( "click" );
       }
-
       else {
+        // if there are no moves to suggest, swift to easy mode
         console.log('moving to easy mode');
         aiLevel = 'easy';
         return createMove();
       };
     }         // aiLevel hard
-  };       // createMove
-
-  window.aI = function (){
-    window.setTimeout(function () {
-      var selection = createMove();
-      $('td#' + selection).trigger( "click" );  // this clicks
-    }, 500);
-  };      // aI
-
-
+  };       // close createMove
+ // a timer to delay the time the computer takes to select a sqaure
+ window.aI = function (){
+   window.setTimeout(function () {
+   var selection = createMove();
+   $('td#' + selection).trigger( "click" );  // this clicks
+   }, 500);
+ };  // close aI
 
 });  // end of code //
